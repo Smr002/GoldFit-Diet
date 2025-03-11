63bf3a1e5d292e0940c6dd8ca7a41a5230c3d53d
Kristi Samara <ksamara22@epoka.edu.al>
2025-03-11 01:56:00 +0100

    Setup the frontend and created the draft for header and navbar

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import CustomHeader from "@/components/CustomHeader";
+import NavBar from "@/components/navbar"
+function App() {
+  return (
+    <>
+      <CustomHeader />
+      <NavBar />
+
+      
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import Box from "@mui/material/Box";
+import Spline from '@splinetool/react-spline';
+
+export default function NavBar() {
+  return (
+    <Box 
+      sx={{
+        position: 'relative',
+        height:'45vw' ,
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 0,
+      }}
+    >
+      <Spline
+      // scene="https://prod.spline.design/lvKHRIond7TKjDk9/scene.splinecode"
+        scene="https://prod.spline.design/T1tZP4lWDfSJgJcJ/scene.splinecode"
+        style={{ width: '100%', height: '100%', maxWidth: '100vw'
+        }}
+      />
+    </Box>
+  );
+}
