27690cb83987d7ad709df6df3cef615b6e11900d
erminlilaj <ermin.lilaj04@gmail.com>
2025-04-24 02:16:55 +0200

    Revert "Merge branch 'frontend-develop' of https://github.com/Smr002/GoldFit-Diet into backend-develop"
    This reverts commit 12b225ed06ad37bcb67821672775030b140403c7, reversing
    changes made to f483ed0409eb071b291e4b416098a90217c4de73.

--- my-react-app/src/components/nutrition/index.js
+++
@@ -1,5 +0,0 @@
-export { default as CalorieTracker } from './CalorieTracker';
-export { default as MacronutrientBreakdown } from './MacronutrientBreakdown';
-export { default as MealLogger } from './MealLogger';
-export { default as WaterTracker } from './WaterTracker';
-export { default as SleepTracker } from './SleepTracker';
--- my-react-app/src/helper.js
+++
@@ -1,9 +0,0 @@
-export function getUserIdFromToken(token) {
-    try {
-      const payload = JSON.parse(atob(token.split('.')[1]));
-      return payload.userId || payload.id || payload.sub; 
-    } catch {
-      return null;
-    }
-  }
-  
